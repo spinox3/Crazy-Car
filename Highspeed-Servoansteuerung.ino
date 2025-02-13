087c85ece99abba9b2f4be744dea4e12420cced7
Marino Batarilo <marino.batarilo99@gmail.com>
2025-02-13 13:49:19 +0100

    Highspeed 1. Testung (funktioniert noch nicht)
    Timer auf 2 MHz geprescaled damit ein Timer-Tick 0.5us dauert
    ICR1 auf 20000 das heißt er zählt einmal 10ms und dann nochmal 10ms also insgesamt 20ms. Das sind 50Hz also perfekt für Steuerung.
    
    Im Loop wollte ich es dann ausprobieren und habe den OCR1A register geändert (1000 = 0.5ms halbe  = 1ms ganze = 0°) auf einen Wert zwischen 1000 und 2000 ticks ontime also 1-2ms von 20ms sind 0-180°. Jedoch geht es nicht Vermutung aber falschen Pin eingstellt.
    
    Bitshifts durchgeführt, PWM eingestellt und versucht mit verschiedenen Werten die Testung durchzuführen, leider nicht funktional.

--- Highspeed-Servoansteuerung.ino
+++ Highspeed-Servoansteuerung.ino
@@ -1,11 +1,22 @@
 // Code von Marino
 
-void setup() {
-  // put your setup code here, to run once:
+#define Servo_Pin PB1  // PB1 entspricht Pin 13 auf deinem ATmega328P
 
+void setup() {
+  // Fast PWM Mode mit ICR1 als TOP-Wert (präzise 50 Hz / 20 ms Periode)
+  TCCR1A = (1 << COM1A1) | (1 << WGM11);  // Clear OC1A on Compare Match, Fast PWM Mode
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8 = 16/8=2MHz, Fast PWM and ICR1 is TOP
+
+  ICR1 = 20000; // setting to 20k TOP, 0.5us is half periodlength meaning *20000 is 10ms, whole Period is then 20ms = 50Hz
+  // Servo motor works with 50Hz = 20ms Period, first 1-2ms on-time determines rotation degree of servo
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  OCR1A = 1000; // 1000 * 0.5us = 0.5ms whole Period = 1ms = 0°
+  delay(1000);
+  OCR1A = 1500;
+  delay(1000);
+  OCR1A = 2000;
+  delay(1000);
 }
