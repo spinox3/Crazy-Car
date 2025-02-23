20ff57a524c7fcccb3268b9fa79f24c2b98c067e
Marino Batarilo <marino.batarilo99@gmail.com>
2025-02-23 15:05:50 +0100

    Arduino Output korrigiert
    Pin 9 als Ausgang gestellt, nun bewegt sich der Motor jedoch dreht er sich nicht wie gewünscht.

--- Highspeed-Servoansteuerung.ino
+++ Highspeed-Servoansteuerung.ino
@@ -1,14 +1,17 @@
-// Code von Marino
-
-#define Servo_Pin PB1  // PB1 entspricht Pin 13 auf deinem ATmega328P
+// Servo Motor controller without Servo.h library
+//@author Marino
+//@version 23.02.2025
 
 void setup() {
+  pinMode(9, OUTPUT); // Pin 9 als Ausgang (OC1A)
+  TCCR1A = 0;
+  TCCR1B = 0;
   // Fast PWM Mode mit ICR1 als TOP-Wert (präzise 50 Hz / 20 ms Periode)
   TCCR1A = (1 << COM1A1) | (1 << WGM11);  // Clear OC1A on Compare Match, Fast PWM Mode
-  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8 = 16/8=2MHz, Fast PWM and ICR1 is TOP
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8 = 16/8=2MHz, Fast PWM mit ICR1 als TOP
 
-  ICR1 = 20000; // setting to 20k TOP, 0.5us is half periodlength meaning *20000 is 10ms, whole Period is then 20ms = 50Hz
-  // Servo motor works with 50Hz = 20ms Period, first 1-2ms on-time determines rotation degree of servo
+  ICR1 = 20000; // setting to 20k TOP, 0.5us is periodlength meaning *20000 is 10ms, whole Period is then 20ms = 50Hz
+  // Servo motor works with 50Hz = 20ms Period; first 1-2ms on-time determines rotation degree of servo
 }
 
 
