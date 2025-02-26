0019ca50252993113d823bf8d273512b3f79ec54
Marino Batarilo <marino.batarilo99@gmail.com>
2025-02-26 16:35:42 +0100

    Timer changed
    Changed Timer1 to Timer4 to ensure compatibility between highspeed servo control and ADC-Interrupts.

--- Highspeed-Servoansteuerung.ino
+++ Highspeed-Servoansteuerung.ino
@@ -4,13 +4,13 @@
 
 void setup() {
   pinMode(9, OUTPUT); // Pin 9 als Ausgang (OC1A)
-  TCCR1A = 0;
-  TCCR1B = 0;
-  // Fast PWM Mode mit ICR1 als TOP-Wert (präzise 50 Hz / 20 ms Periode)
-  TCCR1A = (1 << COM1A1) | (1 << WGM11);  // Clear OC1A on Compare Match, Fast PWM Mode
-  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8 = 16/8=2MHz, Fast PWM mit ICR1 als TOP
+  TCCR4A = 0;
+  TCCR4B = 0;
+  // Fast PWM Mode mit ICR4 als TOP-Wert (präzise 50 Hz / 20 ms Periode)
+  TCCR4A = (1 << COM1A1) | (1 << WGM11);  // Clear OC4A on Compare Match, Fast PWM Mode
+  TCCR4B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8 = 16/8=2MHz, Fast PWM mit ICR4 als TOP
 
-  ICR1 = 20000; // setting to 20k TOP, 0.5us is periodlength meaning *20000 is 10ms, whole Period is then 20ms = 50Hz
+  ICR4 = 20000; // setting to 20k TOP, 0.5us is periodlength meaning *20000 is 10ms, whole Period is then 20ms = 50Hz
   // Servo motor works with 50Hz = 20ms Period; first 1-2ms on-time determines rotation degree of servo
 }
 
@@ -22,6 +22,6 @@ void loop() {
 }
 
 void control(int grad) {
-  OCR1A = map(grad, 0, 180, 1000, 4950); // map correctly to be able to enter degrees
+  OCR4A = map(grad, 0, 180, 1000, 4950); // map correctly to be able to enter degrees
   delay(1000);
 }
