1b82ea9b529539b22d9e0ca6753c398ee6a951af
Marino Batarilo <marino.batarilo99@gmail.com>
2025-02-27 14:02:17 +0100

    Fixed Output Pin of new Timer
    title

--- Highspeed-Servoansteuerung.ino
+++ Highspeed-Servoansteuerung.ino
@@ -1,19 +1,18 @@
-// Servo Motor controller without Servo.h library
-//@author Marino
-//@version 26.02.2025
+// Servo Motor controller without Servo.h library (using Timer4)
+// @author Marino
+// @version 27.02.2025
 
 void setup() {
-  pinMode(9, OUTPUT); // Pin 9 als Ausgang (OC1A)
+  pinMode(6, OUTPUT); // Pin 6 als Ausgang (OC4A)
   TCCR4A = 0;
   TCCR4B = 0;
   // Fast PWM Mode mit ICR4 als TOP-Wert (präzise 50 Hz / 20 ms Periode)
-  TCCR4A = (1 << COM1A1) | (1 << WGM11);  // Clear OC4A on Compare Match, Fast PWM Mode
-  TCCR4B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8 = 16/8=2MHz, Fast PWM mit ICR4 als TOP
+  TCCR4A = (1 << COM4A1) | (1 << WGM41);  // Clear OC4A on Compare Match, Fast PWM Mode
+  TCCR4B = (1 << WGM43) | (1 << WGM42) | (1 << CS41); // Prescaler 8 = 16/8=2MHz, Fast PWM mit ICR4 als TOP
 
   ICR4 = 20000; // setting to 20k TOP, 0.5us is periodlength meaning *20000 is 10ms, whole Period is then 20ms = 50Hz
   // Servo motor works with 50Hz = 20ms Period; first 1-2ms on-time determines rotation degree of servo
 }
-
 
 void loop() {
   control(0); 
